f09304c32a7dd3d36bb818d4a380114c337f9d14
Xavier M. <xavier.mcin@gmail.com>
2025-10-20 14:12:12 -0400

    forms

--- src/components/main_components/Contact.js
+++ src/components/main_components/Contact.js
@@ -2,7 +2,7 @@ import { useForm } from "@formspree/react"
 
 export default function Contact() {
 
-  const [state, handleSubmit, reset] = useForm('https://formspree.io/f/xovkzvvq');
+  const [state, handleSubmit, reset] = useForm('xovkzvvq');
   if (state.succeeded) {
     return <div>Thank you for submitting</div>
   }
@@ -19,13 +19,13 @@ export default function Contact() {
       <div>
         <p>Have I piqued your interest</p>
         <h2>Get In Touch</h2>
-        <form autoComplete="off" onSubmit={handleSubmit} method="POST">
+        <form autoComplete="off" onSubmit={handleSubmit}>
             <input type="text" name="name" id="name" placeholder="Your Name"/>
             <input type="text" name="company" id="company" placeholder="Company Name"/>
             <input type="email" name="email" id="email" placeholder="E-Mail"/>
             <input type="tel" name="phone" id="phone" placeholder="Phone Number" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
             <textarea name="message" id="message" placeholder="Message"></textarea>
-            <button type="submit" disabled={state.submitting}>Send</button>
+            <button type="submit">Send</button>
         </form>
       </div>
 
