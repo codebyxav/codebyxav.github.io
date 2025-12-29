82d7c0dadf198b9d4d0cc7825278363b27c2ca7f
Xavier M. <xavier.mcin@gmail.com>
2025-12-29 10:37:39 -0500

    switched some from flexbox to grid

--- src/components/main_components/Projects.js
+++ src/components/main_components/Projects.js
@@ -1,21 +1,23 @@
 import { useEffect, useState } from 'react';
 import { future, working } from '../../assets/icons';
 import dark from '../../assets/darkgray.png';
+import particle from '../../assets/project_images/particle.gif';
 
 export default function Projects() {
 
   const [proj, setProj] = useState([]);
 
   const cards = [
-    { title: '', status: [working, 'Project In Progress'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
-    { title: '', status: [future, 'Project Not Started'], bgimage: dark},
+    { title: 'Text Particles App', status: [working, 'Project In Progress'], bgimage: particle, url: 'https://codebyxav.github.io/TextParticles'},
+    { title: '', status: [working, 'Project In Progress'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
+    { title: '', status: [future, 'Project Not Started'], bgimage: dark, url: ''},
   ];
+
 
   useEffect(() => {
     setProj(cards);
@@ -38,7 +40,7 @@ export default function Projects() {
 
               { proj.map((value, index) => {
                 return (
-                  <li key={index} data-after-text={value.title} ><span></span><img src={value.bgimage} alt="" /><div>{value.status[0]}<p>{value.status[1]}</p></div></li>
+                  <li key={index} data-after-text={value.title} ><img src={value.bgimage} alt="" /><a href={value.url} target='_blank'></a><div>{value.status[0]}<p>{value.status[1]}</p></div></li>
                 )
               })}
 
